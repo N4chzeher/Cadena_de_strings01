2ee9115371ed67439ba73272b7ca7d2a731594be
n4chzeher <diegorrojas@hotmail.com>
2016-11-15 17:07:36 +0100

    .

--- src/com/company/EX06.java
+++ src/com/company/EX06.java
@@ -1,7 +1,20 @@
 package com.company;
 
-/**
- * Created by NACHZEHER on 15/11/2016.
- */
+// Escriure un programa en llenguatge Java on es demani una frase per teclat i es mostri per
+// pantalla amb tots els caràcters en lletra minúscula.
+
+import java.util.Scanner;
+import java.util.StringJoiner;
+
 public class EX06 {
+
+    public static void main (String [] args){
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.printf("Ingresa una frase: ");
+        String fra1=scanner.nextLine();
+
+        System.out.printf(fra1.toLowerCase());
+    }
+
 }
---
+++ src/com/company/EX07.java
@@ -0,0 +1,12 @@
+package com.company;
+
+// Escriure un programa en llenguatge Java on es demani una frase per teclat i es mostri per
+// pantalla amb tots els caràcters en lletra majúscula.
+
+public class EX07 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
